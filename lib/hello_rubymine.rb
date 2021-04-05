9fc1a8acab7b65cdc866945f256e6c19dd8dd43e
Brett D Stevens <brettdstevens@gmail.com>
2021-04-05 17:18:18 +1000

    push gem

--- lib/hello_rubymine.rb
+++ lib/hello_rubymine.rb
@@ -2,5 +2,12 @@ require "hello_rubymine/version"
 
 module HelloRubymine
   class Error < StandardError; end
-  # Your code goes here...
+  require "hello_rubymine/version"
+
+  module HelloRubymine
+    def self.greet(name)
+      puts "Hello, #{name}! I'm Ruby!"
+    end
+  end
+
 end
